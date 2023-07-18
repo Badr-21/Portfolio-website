88d7f139f50ed9c6f792c09f07266927be32d6a2
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-18 18:00:26 +0200

    added more colors to tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{js,jsx}"],
+   content: ["./index.html", "./src/**/*.{js,jsx}"],
    theme: {
       extend: {
          colors: {
             PictonBlue: "#00ABE4",
             SkyBlue: "#75CEEF",
-            AliceBlue: "#E9F1FA",
+            AliceBlue: "#F4F8FD",
             RichBlack: "#00090B",
          },
       },
