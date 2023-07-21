afb2ce83c5db1949714829dd5d890fc5730b6b43
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-21 23:36:10 +0200

    modified Home.jsx file

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,8 +2,12 @@ import { forwardRef } from "react";
 
 const Home = forwardRef(function Home(props, homeRef) {
    return (
-      <section ref={homeRef} className="h-[50rem]">
-         Home
+      <section
+         ref={homeRef}
+         className="flex justify-center p-8 flex-wrap bg-AliceBlue text-RichBlack"
+      >
+         <p className="text-6xl font-semibold">Hi, I'm Badreddine Boudaoud.</p>
+         <h1 className="text-7xl font-bold text-PictonBlue">A Frontend developer</h1>
       </section>
    );
 });
