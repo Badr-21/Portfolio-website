f3f7448e6de23e99a32601e156e747bd8a8aa7a8
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-27 22:50:45 +0200

    updated Footer component and added responsive styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,31 +3,33 @@ import GithubWhiteIcon from "../assets/github-white-icon.svg";
 
 function Footer() {
    return (
-      <section className="py-12 px-36 flex justify-between bg-DavysGray">
-         <p className="text-xl font-semibold text-white">
+      <section className="py-8 lg:px-36 px-8 flex flex-col items-center gap-y-12 bg-DavysGray">
+         <div className="w-full flex flex-col-reverse justify-between items-center sm:flex-row gap-y-4">
+            <p className="text-white font-medium">badreddine.boudaoud.dev@gmail.com</p>
+            <div className="flex gap-x-4 flex-shrink-0">
+               <a
+                  href="https://www.linkedin.com/in/badreddine-boudaoud-abb61b282/"
+                  target="_blank"
+                  rel="noreferrer"
+               >
+                  <img
+                     className="w-6 [@media(hover:hover){&:hover}]:filter-icon hover:cursor-pointer"
+                     src={LinkedinWhiteIcon}
+                     alt="linkedin icon"
+                  />
+               </a>
+               <a href="https://github.com/Badr-21" target="_blank" rel="noreferrer">
+                  <img
+                     className="w-6 [@media(hover:hover){&:hover}]:filter-icon hover:cursor-pointer"
+                     src={GithubWhiteIcon}
+                     alt="github icon"
+                  />
+               </a>
+            </div>
+         </div>
+         <p className="text-lg font-semibold text-white">
             Designed & build by Badreddine Boudaoud &copy; 2023
          </p>
-         <div className="flex items-center gap-x-4">
-            <p className="text-white font-medium">badreddine.boudaoud.dev@gmail.com</p>
-            <a
-               href="https://www.linkedin.com/in/badreddine-boudaoud-abb61b282/"
-               target="_blank"
-               rel="noreferrer"
-            >
-               <img
-                  className="w-6 [@media(hover:hover){&:hover}]:filter-icon hover:cursor-pointer"
-                  src={LinkedinWhiteIcon}
-                  alt="linkedin icon"
-               />
-            </a>
-            <a href="https://github.com/Badr-21" target="_blank" rel="noreferrer">
-               <img
-                  className="w-6 [@media(hover:hover){&:hover}]:filter-icon hover:cursor-pointer"
-                  src={GithubWhiteIcon}
-                  alt="github icon"
-               />
-            </a>
-         </div>
       </section>
    );
 }
