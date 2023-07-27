9b7cc9f1bd04418238cf98ec0594e0a56b9af4d3
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-28 01:06:36 +0200

    updated Header component and added responsive styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@
 import Logo from "../assets/logo.svg";
 import HamburgerIcon from "../assets/hamburger-icon.svg";
 import CloseIcon from "../assets/close-icon.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function Header({
    homeRef,
@@ -10,59 +10,127 @@ function Header({
    myProjectsRef,
    contactMeRef,
    handleScrollTo,
-   setShow,
-   show,
+   setShowNavBar,
+   showNavBar,
 }) {
    const [lastScrollY, setLastScrollY] = useState(0);
+   const [showSideBar, setShowSideBar] = useState(false);
+   const [responsiveHeader, setResponsiveHeader] = useState(false);
+   const navRef = useRef(null);
+
+   const handleReload = () => {
+      window.location.reload();
+   };
 
    const controlHeader = () => {
-      if (window.scrollY > lastScrollY) {
-         setShow(false);
-      } else {
-         setShow(true);
+      if (!showSideBar) {
+         if (window.scrollY > lastScrollY) {
+            setShowNavBar(false);
+         } else {
+            setShowNavBar(true);
+         }
+         setLastScrollY(window.scrollY);
       }
-      setLastScrollY(window.scrollY);
    };
+
    useEffect(() => {
       window.addEventListener("scroll", controlHeader);
       return () => {
          window.removeEventListener("scroll", controlHeader);
       };
    }, [lastScrollY]);
+
+   const handleSideBar = () => {
+      setShowSideBar(!showSideBar);
+   };
+
+   useEffect(() => {
+      const handleResponsiveHeader = () => {
+         if (window.innerWidth < 640) {
+            setResponsiveHeader(true);
+         } else {
+            setResponsiveHeader(false);
+         }
+      };
+      handleResponsiveHeader();
+      window.addEventListener("resize", handleResponsiveHeader);
+      return () => window.removeEventListener("rezise", handleResponsiveHeader);
+   }, []);
+
+   // useEffect(() => {
+   //    const closeSideBar = (e) => {
+   //       if (!navRef.current?.contains(e.target)) {
+   //          setShowSideBar(false);
+   //       }
+   //    };
+   //    document.body.addEventListener("click", closeSideBar, true);
+   //    return () => document.body.removeEventListener("click", closeSideBar, true);
+   // });
+
    return (
       <section
          className={`${
-            show ? "top-0" : "top-[-5rem]"
-         } w-full flex justify-between items-center py-4 px-8 shadow sticky transition-top duration-300 bg-white z-10`}
+            showNavBar ? "top-0" : "top-[-5rem]"
+         }  w-full flex justify-between items-center py-6 px-8 shadow sticky transition-top duration-300 bg-white z-10`}
       >
-         <img src={Logo} alt="logo" className="w-[10rem]" />
-         <img src={HamburgerIcon} alt="hamburger icon" className="hidden" />
-         <nav className="flex items-center gap-x-6 text-PictonBlue text-lg font-semibold">
-            <img src={CloseIcon} alt="close icon" className="hidden" />
-            <p
-               className="hover:cursor-pointer hover:text-SkyBlue"
-               onClick={() => handleScrollTo(homeRef)}
-            >
-               Home
-            </p>
-            <p
-               className="hover:cursor-pointer hover:text-SkyBlue"
-               onClick={() => handleScrollTo(aboutMeRef)}
-            >
-               About Me
-            </p>
-            <p
-               className="hover:cursor-pointer hover:text-SkyBlue"
-               onClick={() => handleScrollTo(myProjectsRef)}
-            >
-               My Project
-            </p>
-            <p
-               className="hover:cursor-pointer hover:text-SkyBlue"
-               onClick={() => handleScrollTo(contactMeRef)}
-            >
-               Contact Me
-            </p>
+         <img
+            src={HamburgerIcon}
+            alt="hamburger icon"
+            className={`${responsiveHeader ? "" : "hidden"} w-8 hover:cursor-pointer`}
+            onClick={handleSideBar}
+         />
+         <img
+            src={Logo}
+            alt="logo"
+            className="w-[10rem] hover:cursor-pointer"
+            onClick={handleReload}
+         />
+         <nav
+            ref={navRef}
+            className={`${
+               showSideBar ? "left-[0]" : "left-[-100%]"
+            } w-full h-screen p-8 sm:w-fit transition-left absolute top-0 flex flex-col items-center gap-y-8 duration-300 sm:static sm:shadow-none bg-white`}
+         >
+            <img
+               src={CloseIcon}
+               alt="close icon"
+               className={`${responsiveHeader ? "" : "hidden"} w-8 hover:cursor-pointer self-start`}
+               onClick={handleSideBar}
+            />
+            <ul className="flex text-lg gap-x-6 text-PictonBlue font-semibold sm:flex-row flex-col sm:gap-y-0 gap-y-16">
+               <p
+                  className="hover:cursor-pointer [@media(hover:hover){&:hover}]:text-SkyBlue"
+                  onClick={() => {
+                     handleScrollTo(homeRef), handleSideBar();
+                  }}
+               >
+                  Home
+               </p>
+               <p
+                  className="hover:cursor-pointer hover:text-SkyBlue"
+                  onClick={() => {
+                     handleScrollTo(aboutMeRef), handleSideBar();
+                  }}
+               >
+                  About Me
+               </p>
+               <p
+                  className="hover:cursor-pointer hover:text-SkyBlue"
+                  onClick={() => {
+                     handleScrollTo(myProjectsRef), handleSideBar();
+                  }}
+               >
+                  My Project
+               </p>
+               <p
+                  className="hover:cursor-pointer hover:text-SkyBlue"
+                  onClick={() => {
+                     handleScrollTo(contactMeRef), handleSideBar();
+                  }}
+               >
+                  Contact Me
+               </p>
+            </ul>
          </nav>
       </section>
    );
