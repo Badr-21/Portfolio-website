99f7e0336be7e1546bddd28e85c0d7333d790b03
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-31 15:20:30 +0200

    updated MyProjects component styles

--- src/Components/MyProjects.jsx
+++ src/Components/MyProjects.jsx
@@ -9,12 +9,12 @@ const MyProjects = forwardRef(function MyProjects(props, myProjectsRef) {
    return (
       <section
          ref={myProjectsRef}
-         className="py-16 w-full bg-AliceBlue flex flex-col gap-y-20 items-center"
+         className="py-16 w-full bg-AliceBlue flex flex-col gap-y-20 items-center overflow-x-hidden"
       >
          <h2 className=" w-[60rem] text-4xl text-PictonBlue font-semibold self-start lg:px-36 px-8">
             My Projects
          </h2>
-         <article className="lg:w-[60rem] lg:h-[25rem] md:w-[40rem] sm:w-[35rem] min-[445px]:w-[25rem] h-[35rem] w-[18rem] border flex lg:flex-row flex-col overflow-hidden p-4 rounded-md shadow bg-white">
+         <article className="lg:w-[60rem] lg:h-[26rem] md:w-[40rem] sm:w-[35rem] min-[445px]:w-[25rem] h-[35rem] w-[18rem] border flex lg:flex-row flex-col overflow-hidden p-4 rounded-md shadow bg-white">
             <div className="md:w-[38rem] sm:w-[33rem] min-[445px]:w-[23rem] w-[16rem] border rounded-md overflow-hidden">
                <a href="https://badr-21-platinum-gym.netlify.app/" target="_blank" rel="noreferrer">
                   <img
@@ -68,7 +68,7 @@ const MyProjects = forwardRef(function MyProjects(props, myProjectsRef) {
                </div>
             </div>
          </article>
-         <article className="lg:w-[60rem] lg:h-[25rem] md:w-[40rem] sm:w-[35rem] min-[445px]:w-[25rem] h-[35rem] w-[18rem] border flex lg:flex-row flex-col overflow-hidden p-4 rounded-md shadow bg-white">
+         <article className="lg:w-[60rem] lg:h-[26rem] md:w-[40rem] sm:w-[35rem] min-[445px]:w-[25rem] h-[35rem] w-[18rem] border flex lg:flex-row flex-col overflow-hidden p-4 rounded-md shadow bg-white">
             <div className="md:w-[38rem] sm:w-[33rem] min-[445px]:w-[23rem] w-[16rem] border rounded-md overflow-hidden">
                <a href="https://badr-21-book-tracker.netlify.app/" target="_blank" rel="noreferrer">
                   <img
@@ -120,7 +120,7 @@ const MyProjects = forwardRef(function MyProjects(props, myProjectsRef) {
                </div>
             </div>
          </article>
-         <article className="lg:w-[60rem] lg:h-[25rem] md:w-[40rem] sm:w-[35rem] min-[445px]:w-[25rem] h-[35rem] w-[18rem] border flex lg:flex-row flex-col overflow-hidden p-4 rounded-md shadow bg-white">
+         <article className="lg:w-[60rem] lg:h-[26rem] md:w-[40rem] sm:w-[35rem] min-[445px]:w-[25rem] h-[35rem] w-[18rem] border flex lg:flex-row flex-col overflow-hidden p-4 rounded-md shadow bg-white">
             <div className="md:w-[38rem] sm:w-[33rem] min-[445px]:w-[23rem] w-[16rem] border rounded-md overflow-hidden">
                <a
                   href="https://badr-21-employment-website.netlify.app/"
@@ -137,9 +137,9 @@ const MyProjects = forwardRef(function MyProjects(props, myProjectsRef) {
                <h3 className="text-PictonBlue font-bold text-center mb-4">Job Hunt</h3>
                <p className="text-DavysGray font-medium mb-4 max-[445px]:text-sm">
                   JobHunt is an employment website. A job search website using data extracted from
-                  The muse api and stored in a JSON file. You can search for the job using job title
-                  or keyword, or you can search for the location using a city, a state, or a country
-                  name, you can also use remote instead of any location.
+                  The muse api and stored in a JSON file. You can search for the a job using job
+                  title or keyword, or you can search for the location using a city, a state, or a
+                  country name, you can also use remote instead of any location.
                </p>
                <div className="flex gap-x-4 mb-4">
                   <p className="shadow border rounded-md p-1 font-semibold">React</p>
